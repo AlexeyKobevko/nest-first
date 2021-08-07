d70ed2c50310167971d20db4e08c135f4c9a9c71
Jedi <folco_2000@mail.ru>
2021-08-07 17:48:56 +0300

    add auth

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -68,6 +68,10 @@ export class UserService {
     return userByEmail;
   }
 
+  findById(id: number): Promise<UserEntity> {
+    return this.userRepository.findOne(id);
+  }
+
   generateJwt(user: UserEntity): string {
     return sign(
       {
