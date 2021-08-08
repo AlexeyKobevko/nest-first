59722b12fdc3392a0784811b1da2553c0b5bc254
Jedi <folco_2000@mail.ru>
2021-08-08 16:29:02 +0300

    add follow

---
+++ src/migrations/1628425666573-CreateFollows.ts
@@ -0,0 +1,15 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CreateFollows1628425666573 implements MigrationInterface {
+  name = 'CreateFollows1628425666573';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "follows" ("id" SERIAL NOT NULL, "followerId" integer NOT NULL, "followingId" integer NOT NULL, CONSTRAINT "PK_fda88bc28a84d2d6d06e19df6e5" PRIMARY KEY ("id"))`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE "follows"`);
+  }
+}
